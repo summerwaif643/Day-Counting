24f53286def2e6e769b26f1de19fea0f1a0cef1a
summerwaif643 <summerwaif643@gmail.com>
2022-07-17 18:46:32 +0000

    modal frontend finished

--- src/app/homepage/homepage.page.ts
+++ src/app/homepage/homepage.page.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core';
+import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.page.html',
   styleUrls: ['./homepage.page.scss'],
 })
+
 export class HomepagePage implements OnInit {
+  @ViewChild(IonModal) modal: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string;
+  date: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+    console.log(this.name, this.date)
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 
   constructor() { }
 
